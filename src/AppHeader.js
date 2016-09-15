a0a8c0a7ada480a04ae30804439bd0b885a4feba
Rong <rongster3544@gmail.com>
2016-09-14 20:51:11 -0700

    Add subreddit headers

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -7,8 +7,13 @@ var AppHeader = (props)=>{
     <div>
       <Link to={{pathname:'/r/AskReddit'}}> AskReddit </Link>
       <Link to={{pathname:'/r/Funny'}}> Funny </Link>
+      <Link to={{pathname:'/r/Vidoes'}}> Videos </Link>
+      <Link to={{pathname:'/r/WorldNews'}}> WorldNews </Link>
+      <Link to={{pathname:'/r/TodayILearned'}}> TodayILearned </Link>
       <Link to={{pathname:'/r/Pics'}}> Pics </Link>
       <Link to={{pathname:'/r/Gifs'}}> Gifs </Link>
+      <Link to={{pathname:'/r/News'}}> News </Link>
+
       {props.children}
     </div>
   )
